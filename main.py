9d57df870cf11d9f6d55d5eba205b6bdcf8964b2
rinalex <eugene.rinaldi@abbvie.com>
2021-09-06 17:37:42 -0500

    Splunk add-on builder helper functions adjust POC code to read splunk events

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ cfg.read('./config.ini')
 bs = cfg.get('default','bootstrap_servers').split(',')
 dest_topic = cfg.get('default','topic')
 
-producer = KafkaProducer(bootstrap_servers = bs, value_serializer=lambda x:dumps(x).encode('utf-8'), retries=5)
 
-for e in range(10,100):
-    data = {'number' : e}
-    producer.send(dest_topic, value=data)
-    print(data)
-    sleep(.01)
+
+#splunk add-on builder helper functions
+def process_event(helper, *args, **kwargs):
+
+    setup_param_bootstrap_servers = helper.get_global_setting("bootstrap_servers").split(',')
+    alert_action_param_topic = helper.get_param("kafka_topic")
+    helper.log_info('setup_param_bootstrap_servers={}'.format(setup_param_bootstrap_servers))
+    helper.log_info('kafka_topic={}'.format(alert_action_param_topic))
+
+    events = helper.get_events()
+    producer = KafkaProducer(bootstrap_servers = bs, value_serializer=lambda x:dumps(x).encode('utf-8'), retries=5)
+
+    for event in events:
+        producer.send(dest_topic, value=event)
+        sleep(.01)
+
+    return 0
